5c201f59aff70789aecf4e837bd127d75b6503a0
Pranay Kumar <pc2282001@gmail.com>
2021-05-14 15:36:54 +0530

    page404 livesearch login signup bug crrted icons bs4

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,10 +1,9 @@
-import {UPDATE_POSTS} from '../actions/actionType'
-export default function posts(state=[],action){
-    switch(action.type){
-        case UPDATE_POSTS:
-            return action.posts
-        default:
-            return state;
-    }
-    
+import { UPDATE_POSTS } from '../actions/actionType';
+export default function posts(state = [], action) {
+  switch (action.type) {
+    case UPDATE_POSTS:
+      return action.posts;
+    default:
+      return state;
+  }
 }
